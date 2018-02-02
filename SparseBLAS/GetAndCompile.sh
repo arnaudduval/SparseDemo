62d8aad11f25dd447f519c5d423b165b2b2ffd56
Arnaud Duval <arnaud.duval@gmail.com>
2018-02-02 17:29:42 +0100

    Add build script to get a version of Sparse BLAS and build it with CMake Still some compilation errors to fix

--- SparseBLAS/GetAndCompile.sh
+++ SparseBLAS/GetAndCompile.sh
@@ -8,3 +8,9 @@ rm 818
 sed -i -e "s/FFLAGS = -g/FFLAGS = -O3/g" SOFTWARE/Makefile
 sed -i -e "s/FC = f90/FC = gfortran/g" SOFTWARE/Makefile
 sed -i -e "s/CC = cc/CC = gcc/g" SOFTWARE/Makefile
+
+rm -rf INSTALL README.1st SPEC_ARITH TESTER TARGET_FILES SOURCE_FILES
+
+mv SOFTWARE src
+
+
