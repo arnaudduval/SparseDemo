5e643c9862eb290624c9a416672e9fe793a56278
Arnaud Duval <arnaud.duval@gmail.com>
2018-02-12 17:03:15 +0100

    Add a basic example for PETSc

---
+++ petsc-example.f90
@@ -0,0 +1,20 @@
+!! Basic Petsc example
+!! compile with gfortran petsc-example.f90 -I/usr/lib/petscdir/3.7.6/x86_64-linux-gnu-real/include/petsc/ -I/usr/lib/petscdir/3.7.6/x86_64-linux-gnu-real/include -I/usr/lib/petscdir/3.7.6/x86_64-linux-gnu-real/include/petsc/mpiuni -cpp -lpetsc -o petsc-example
+
+program main
+
+    implicit none
+    
+#include "finclude/petscsys.h"
+#include "finclude/petscvec.h"
+
+    PetscErrorCode ierr
+    Vec x
+    
+    call PetscInitialize(PETSC_NULL_CHARACTER, ierr)
+    call VecCreateSeq(PETSC_COMM_SELF, 100, x, ierr)
+    call VecSet(x, 1., ierr)
+    call PetscFinalize(ierr)
+    
+
+end program main
