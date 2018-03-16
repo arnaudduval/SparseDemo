0a9cae296f90340e3c13c962557621124e2d5f8f
Arnaud Duval <arnaud.duval@gmail.com>
2018-03-16 10:55:01 +0100

    Improve matrix assembly and export to a Python module

--- assembly.f90
+++ assembly.f90
@@ -1,47 +1,63 @@
 !! Programme qui permet de faire l'assemblage d'une matrice type raideur
 
 module buildMat
-    
+#include <petsc/finclude/petscmat.h>
+    use petscmat    
     real(8), pointer :: pA(:)
     integer, pointer :: pia(:), pja(:)
+    Mat :: stiffness
+    
 contains
 
-subroutine Assemble(connectivity, nElts, nNodes, nNodePerElement, stiffness)
+subroutine Element(connectivity, nElts, nNodes, nNodePerElement, iElement, m, n, v)
+    integer, intent(in) :: nElts, nNodes, nNodePerElement, iElement
+    integer, dimension(nElts, nNodePerElement+1), intent(in) :: connectivity
+    
+    integer, dimension(nNodePerElement), intent(out) :: m, n
+    double precision, dimension(nNodePerElement*nNodePerElement), intent(out) :: v
+    
+    integer i, j, idx
+    
+    do i = 1, nNodePerElement
+        do j = 1, nNodePerElement
+            idx = j + (i-1)*nNodePerElement
+            v(idx) = 1.0
+            m(idx) = connectivity(iElement, i+1)-1
+            n(idx) = connectivity(iElement, j+1)-1
+        enddo
+    enddo
+
+end subroutine Element
+
+subroutine Assemble(connectivity, nElts, nNodes, nNodePerElement, Dstiffness)
     implicit none
     integer, intent(in)                                         :: nElts, nNodePerElement, nNodes
     integer,dimension(nElts, nNodePerElement+1), intent(in)     :: connectivity
-    double precision, dimension(nNodes, nNodes), intent(out)      :: stiffness
+    double precision, dimension(nNodes, nNodes), intent(out)      :: Dstiffness
     
     
     integer :: i, j, iElement
     
-    stiffness = 0.0
+    Dstiffness = 0.0
     
     do iElement = 1, nElts
         do i = 1, nNodePerElement
             do j = 1, nNodePerElement
-                stiffness(connectivity(iElement, i+1), connectivity(iElement, j+1)) &
-                         & = stiffness(connectivity(iElement, i+1), connectivity(iElement, j+1)) + 1.0
+                Dstiffness(connectivity(iElement, i+1), connectivity(iElement, j+1)) &
+                         & = Dstiffness(connectivity(iElement, i+1), connectivity(iElement, j+1)) + 1.0
             enddo
         enddo
     enddo
     
 end subroutine Assemble
 
-subroutine AssemblePETSc(connectivity, nElts, nNodes, nNodePerElement)
+subroutine AssemblePETSc(connectivity, nElts, nNodes, nNodePerElement, nz)
 #include <petsc/finclude/petscmat.h>
     use petscmat
-    use iso_c_binding
     
     implicit none
-    integer, intent(in) :: nElts, nNodePerElement, nNodes
+    integer, intent(in) :: nElts, nNodePerElement, nNodes, nz
     integer, dimension(nElts, nNodePerElement+1), intent(in)    :: connectivity
-
-    
-
-
-    !! Variable locale pour le moment
-    Mat stiffness, B
     
     integer :: i, j, iElement, cpt
     PetscErrorCode :: ierr
@@ -54,11 +70,10 @@ subroutine AssemblePETSc(connectivity, nElts, nNodes, nNodePerElement)
     one = 1.0
     
     !! Initialisation la matrice
-    !!call MatCreateSeqAIJ(PETSC_COMM_SELF, nNodes, nNodes, PETSC_DEFAULT_INTEGER, PETSC_NULL_INTEGER, stiffness, ierr);CHKERRA(ierr)
-    call MatCreateSeqAIJ(PETSC_COMM_SELF, nNodes, nNodes, 500, PETSC_NULL_INTEGER, stiffness, ierr);CHKERRA(ierr)
+    !! nz = nombre de valeur non nulles sur chaque ligne, utilisé pour la préallocation
+    call MatCreateSeqAIJ(PETSC_COMM_SELF, nNodes, nNodes, nz, PETSC_NULL_INTEGER, stiffness, ierr);CHKERRA(ierr)
     
     do iElement = 1, nElts
-        write(*,*) iElement, '/', nElts
         do i = 1, nNodePerElement
             do j = 1, nNodePerElement
                 !! -1 sur les indices car on fait du stockage qui commence à l'indice 0
@@ -71,34 +86,100 @@ subroutine AssemblePETSc(connectivity, nElts, nNodes, nNodePerElement)
     !! Assemblage
     call MatAssemblyBegin(stiffness, MAT_FINAL_ASSEMBLY, ierr);CHKERRA(ierr)
     call MatAssemblyEnd(stiffness, MAT_FINAL_ASSEMBLY, ierr);CHKERRA(ierr)
-    
-   
-    call MatSeqAIJGetArrayF90(stiffness, pA, ierr)
-    !!write(*,*) "array"
-    !!write(*,100) A
-    call MatGetRowIJF90(stiffness, 0, PETSC_FALSE, PETSC_TRUE, n, pIA, pJA, done, ierr)
-    
-    write(*,*) done
-    !!write(*,*) 'n', n
-    !!write(*,*) 'IA', ia
-    !!write(*,*) 'JA', ja
-    
-    
-    
-    call MatCreateSeqAIJWithArrays(PETSC_COMM_SELF, nNodes, nNodes, pIA, pJA, pA, B, ierr)
-    write(*,*) lbound(pA), ubound(pA)
-
-    
-    !!A = pA
-    !!IA = pIA
-    !!JA = pJA
-  
-    call MatEqual(stiffness, B, done, ierr)
-    write(*,*) "matrices egales", done
-    
- 100 format( 2000(F3.1,'   '))
 
 end subroutine AssemblePETSc
+
+
+subroutine AssemblePETScBlock(connectivity, nElts, nNodes, nNodePerElement, nz)
+#include <petsc/finclude/petscmat.h>
+    use petscmat
+    
+    implicit none
+    integer, intent(in) :: nElts, nNodePerElement, nNodes, nz
+    integer, dimension(nElts, nNodePerElement+1), intent(in) :: connectivity
+    
+    integer :: iElement, cpt
+    PetscErrorCode :: ierr
+    PetscInt :: n
+   
+    double precision, dimension(nNodePerElement*nNodePerElement) :: val
+    integer, dimension(nNodePerElement) :: II, JJ
+    
+        !! Initialisation la matrice
+    !! nz = nombre de valeur non nulles sur chaque ligne, utilisé pour la préallocation
+    call MatCreateSeqAIJ(PETSC_COMM_SELF, nNodes, nNodes, nz, PETSC_NULL_INTEGER, stiffness, ierr);CHKERRA(ierr)
+    
+    do iElement = 1, nElts
+        call Element(connectivity, nElts, nNodes, nNodePerElement, iElement, II, JJ, val)    
+        call MatSetValues(stiffness, nNodePerElement, II, nNodePerElement, JJ, val, ADD_VALUES, ierr);CHKERRA(ierr)
+
+    enddo 
+    
+    !! Assemblage
+    call MatAssemblyBegin(stiffness, MAT_FINAL_ASSEMBLY, ierr);CHKERRA(ierr)
+    call MatAssemblyEnd(stiffness, MAT_FINAL_ASSEMBLY, ierr);CHKERRA(ierr)
+    
+end subroutine AssemblePETScBlock
+
+subroutine ExtractCSRStructure
+    PetscErrorCode  :: ierr
+    PetscBool       :: done
+    PetscInt        :: n
+    
+    call MatSeqAIJGetArrayF90(stiffness, pA, ierr)
+    call MatGetRowIJF90(stiffness, 0, PETSC_FALSE, PETSC_TRUE, n, pIA, pJA, done, ierr)
+
+end subroutine ExtractCSRStructure
+
+subroutine GetValue(i,j,v)
+    integer, intent(in) :: i, j
+    real(8), intent(out) :: v
+    
+    integer :: ii(1), jj(1)
+    real(8) :: vv(1)
+    
+    integer::  ierr
+    
+    ii(1) = i
+    jj(1) = j
+    
+    
+    call MatGetValues(stiffness, 1, ii, 1, jj, vv, ierr)
+
+    v = vv(1)
+end subroutine GetValue
+
+subroutine GetNRows(nrows)
+    implicit none
+    integer, intent(out) :: nrows
+    integer              :: ncols
+    PetscErrorCode       :: ierr
+    
+    call MatGetSize(stiffness, nrows, ncols, ierr)
+
+end subroutine GetNRows
+
+subroutine GetNz(nz)
+    integer, intent(out) :: nz
+    
+    nz = ubound(pA,1)
+
+end subroutine GetNz
+
+subroutine GetCSR(nz, nrows, A, IA, JA)
+    integer, intent(in) :: nz
+    integer, intent(in) :: nrows
+    real(8), dimension(nz), intent(out) :: A
+    integer, dimension(nrows+1), intent(out) :: IA
+    integer, dimension(nz), intent(out) :: JA
+    
+    A = pA
+    IA = pIA
+    JA = pJA
+
+
+end subroutine GetCSR
+
 
 subroutine StartPETSc
 #include <petsc/finclude/petscsys.h>
@@ -126,7 +207,7 @@ program test
     integer, allocatable, dimension(:,:) :: connectivity
     integer :: nNodes, nNodePerElement, nElts
     integer :: i, j, dummy
-    double precision, allocatable :: stiffness(:,:)
+    double precision, allocatable :: Dstiffness(:,:)
     
     real(PETSC_REAL_KIND), allocatable :: A(:)
     integer, allocatable :: ia(:), ja(:)
@@ -160,7 +241,7 @@ program test
     endif
     
 
-    call AssemblePETSc(connectivity, nElts, nNodes, nNodePerElement)
+    call AssemblePETSc(connectivity, nElts, nNodes, nNodePerElement, 500)
     
     !!deallocate(stiffness, connectivity)
     
